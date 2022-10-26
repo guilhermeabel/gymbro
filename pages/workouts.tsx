23d6ba1b8ee344cb70494db193f1fd09e3dfb8f8
Guilherme Abel <guilhermeabel8@gmail.com>
2022-10-25 23:32:18 -0300

    Create Workout page

---
+++ pages/workouts.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import styles from '../styles/Home.module.css'
+
+const Workouts: NextPage = () => {
+	return (
+		<div className={styles.container}>
+
+		</div>
+	);
+}
